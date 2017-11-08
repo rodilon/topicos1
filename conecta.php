516637723839b36af7bf99021cd6e470796fa214
Renan Odilon <renanodilon@gmail.com>
2017-11-08 14:48:52 -0200

    Conexão com o banco de dados do hostinger

--- conecta.php
+++ conecta.php
@@ -1,3 +1,3 @@
 <?php
-#$conexao = mysqli_connect('mysql.hostinger.com.br','u760712647_loka','lojaloja','u760712647_loja');
-$conexao = mysqli_connect('localhost','root','','educa-mais');
+$conexao = mysqli_connect('mysql.hostinger.com.br','u760712647_loka','lojaloja','u760712647_loja');
+#$conexao = mysqli_connect('localhost','root','','educa-mais');
